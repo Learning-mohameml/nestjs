1201b6abe1282c0bce75cb3683146bcb17a3c2b4
mohameml <mhmdahmdnbyl18@gmail.com>
2026-02-01 15:28:58 +0100

    update

--- Projets/02_task_app/task-api/src/tasks/tasks.service.ts
+++ Projets/02_task_app/task-api/src/tasks/tasks.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -10,15 +12,65 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, description: string): Task {
+  getTaskWithFilters(filterDto: GetTasksFilterDto): Task[] {
+    const { status, search } = filterDto;
+    let tasks: Task[] = this.getAllTasks();
+
+    if (status) {
+      tasks = tasks.filter((task) => task.status === status);
+    }
+
+    if (search) {
+      tasks = tasks.filter(
+        (task) =>
+          task.titlte.includes(search) || task.description.includes(search),
+      );
+    }
+
+    return tasks;
+  }
+
+  getTaskById(id: string): Task {
+    const task = this.tasks.find((task) => task.id === id);
+
+    if (!task) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+
+    return task;
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
     const task: Task = {
       id: uuid(),
-      titlte: title,
-      description,
+      titlte: createTaskDto.title,
+      description: createTaskDto.description,
       status: TaskStatus.OPEN,
     };
 
     this.tasks.push(task);
+
+    return task;
+  }
+
+  deleteTask(id: string): void {
+    const index = this.tasks.findIndex((task) => task.id === id);
+
+    if (index === -1) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+
+    this.tasks.splice(index, 1);
+  }
+
+  updateTask(id: string, status: TaskStatus): Task {
+    const task = this.tasks.find((task) => task.id === id);
+
+    if (!task) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+
+    task.status = status;
 
     return task;
   }
